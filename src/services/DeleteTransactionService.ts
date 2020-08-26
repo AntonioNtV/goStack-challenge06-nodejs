9829c80ca5f024058ae7d8879558fda35a198b4a
AntonioNtV <antoniontv18@gmail.com>
2020-08-26 20:27:26 -0300

    Service de remocao de uma transaction

--- src/services/DeleteTransactionService.ts
+++ src/services/DeleteTransactionService.ts
@@ -1,8 +1,14 @@
-// import AppError from '../errors/AppError';
+import { getRepository } from 'typeorm';
+import Transaction from '../models/Transaction';
 
+interface Request {
+  transaction_id: string;
+}
 class DeleteTransactionService {
-  public async execute(): Promise<void> {
-    // TODO
+  public async execute({ transaction_id }: Request): Promise<void> {
+    const transactionRepository = getRepository(Transaction);
+
+    await transactionRepository.delete({ id: transaction_id });
   }
 }
 
